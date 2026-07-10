8c49fb98afaa3b83df1e45fc60120db2e8e391fa
maluvicentini <vicentinimalu1@gmail.com>
2026-07-10 15:08:05 -0300

    feat: add icones na sidebar

--- src/assets/componentes/Sidebar.jsx
+++ src/assets/componentes/Sidebar.jsx
@@ -1,12 +1,28 @@
-import '../../App.css'
+import "../../App.css";
+import { Star, Check, CheckBoxRounded } from '@mui/icons-material'
 
 function Sidebar() {
-    return (
-        <nav className="nav-bar">
-            <button className='btn-principal'>Todas</button>
-            <button className='btn-principal'>Completas</button>
-            <button className='btn-principal'>Favoritas</button>
-        </nav>
-    )
+  return (
+    <nav className="nav-bar">
+        <button className="btn-principal">
+            <section>
+                <CheckBoxRounded></CheckBoxRounded>
+                Todas
+            </section>
+        </button>
+        <button className="btn-principal">
+            <section>
+                <Check></Check>
+                Completas
+            </section>
+        </button>
+        <button className="btn-principal">
+            <section>
+                <Star></Star>
+                Favoritas
+            </section>
+        </button>
+    </nav>
+  );
 }
 export default Sidebar;
